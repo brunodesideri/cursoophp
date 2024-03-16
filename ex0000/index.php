b931dcfb490d42ba5eba2bcac6cfae68ed292aea
Bruno Desideri <150479044+brunodesideri@users.noreply.github.com>
2024-03-16 20:27:48 -0300

    PHP

---
+++ ex0000/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo PHP</title>
+</head>
+<body>
+    <h1>Exemplo PHP</h1>
+    <?php
+    $nome = "Bruno Desideri";
+    echo "<p> O meu nome é $nome </p>"
+    ?>
+</body>
+</html>
