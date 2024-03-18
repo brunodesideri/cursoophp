8ea4b5410d7111bb1ef0d683aacb7437eb4fe78a
Bruno Desideri <150479044+brunodesideri@users.noreply.github.com>
2024-03-18 20:24:52 -0300

    Create index.php

---
+++ 0rascunho/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rascunho</title>
+</head>
+<body>
+    <?php
+    $nome = "Rodrigo";
+    $sobrenome = "Nogueira";
+    const NOME = "Bruno";
+    echo "Olá\u{1F596}, ".NOME;
+    echo "! <br>"; 
+    echo " <br> "; 
+    echo "$nome \"Minotauro\" $sobrenome";
+    ?>
+</body>
+</html>
